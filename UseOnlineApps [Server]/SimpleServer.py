ec9a9730dab0f06266bd940d9f54eed37bee11c4
NtLitvinenko <114810424+NtLitvinenko@users.noreply.github.com>
2022-10-01 14:41:42 +0300

    Add comment

--- UseOnlineApps [Server]/SimpleServer.py
+++ UseOnlineApps [Server]/SimpleServer.py
@@ -4,7 +4,7 @@ main = flask.Flask("aaa")
 
 @main.route("/getApp")
 def get():
-    ah = open("AppCode.app","r").read()
+    ah = open("AppCode.app","r").read() #read code for AppCode.app and send to client
     return ah
 
 main.run(port=41)
